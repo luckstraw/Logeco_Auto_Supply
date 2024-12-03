c3f7e7fde645da349ee429e20bdca3a41d8239fa
luckstraw <your_email@example.com>
2024-12-03 20:07:59 +0800

    Homepage progress

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from "vuex";
 import colors from "./modules/colors";
 import footer from "./modules/footer";
+import homepage from "./modules/homepage";
 
 export default createStore({
   modules: {
     colors,
     footer,
+    homepage,
   },
 });
