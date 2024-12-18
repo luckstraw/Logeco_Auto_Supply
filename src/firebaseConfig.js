13908cf0b72c799c64288d43ed4b8e5342f4b586
James Luck Aviles <137030726+luckstraw@users.noreply.github.com>
2024-12-18 20:41:46 +0800

    debug

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -13,6 +13,8 @@ const firebaseConfig = {
   appId: process.env.VUE_APP_FIREBASE_APP_ID,
 };
 
+console.log('API Key:', process.env.VUE_APP_FIREBASE_API_KEY);  // Remove this in production
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
