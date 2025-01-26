d4ffe0badf46086911da5a40647c2d34954dc09a
luckstraw <aviles.jamesluck.ecc.bscs@gmail.com>
2025-01-26 21:43:38 +0800

    Final Admin View

--- src/components/admin/AdminChat.vue
+++ src/components/admin/AdminChat.vue
@@ -1,139 +1,154 @@
 <template>
-  <v-row class="ma-0 justify-space-around">
-    <v-col cols="7" class="ma-0 pa-0">
-      <v-card class="d-flex flex-column fill-height rounded-xl">
-        <v-card-title class="text-center font-weight-bold">
-          Admin Chat List
-        </v-card-title>
-        <v-list lines="two" class="chat-list pa-2 pl-4">
-          <v-list-item
-            v-for="chat in chatList"
-            :key="chat.id"
-            :active="selectedChat?.id === chat.id"
-            @click="selectChat(chat)"
-            :title="chat.name"
-            rounded="xl"
-            class="my-2"
-          >
-            <template v-slot:prepend>
-              <v-avatar
-                :image="chat.avatar || 'https://i.imgur.com/zsEiYcN.png'"
-              />
-            </template>
-            <template v-slot:subtitle>
-              <div class="subtitle-container">
-                <span
-                  :style="{ color: getTextColor(chat) }"
-                  class="subtitle-text"
-                >
-                  {{
-                    chat.lastMessage.senderId === "admin"
-                      ? `You: ${chat.lastMessage.text}`
-                      : chat.lastMessage.text
-                  }}
-                </span>
-                <span
-                  :style="{ color: getTextColor(chat) }"
-                  class="subtitle-time"
-                >
-                  {{
-                    chat.lastMessage.timestamp
-                      ? new Date(
-                          chat.lastMessage.timestamp.seconds * 1000
-                        ).toLocaleString()
-                      : " "
-                  }}
-                </span>
-              </div>
-            </template>
-          </v-list-item>
-        </v-list>
-      </v-card>
-    </v-col>
-    <v-col cols="3" class="d-flex flex-column pa-0">
-      <v-card
-        class="d-flex align-center justify-center rounded-xl mb-3 pa-0"
-        style="flex: 4; position: relative"
-      >
-        <v-row class="ma-0 pa-2">
-          <v-col cols="4" class="ma-0">
-            <v-avatar
-              :image="selectedChat.avatar || 'https://i.imgur.com/zsEiYcN.png'"
-              size="80"
-              :style="{ border: `2px solid ${color.secondary}` }"
-            ></v-avatar>
-          </v-col>
-          <v-col cols="8" class="d-flex align-center">
-            <h3>{{ selectedUser.displayName }}</h3>
-          </v-col>
-          <v-col cols="12" class="overflow-auto">
-            <h4>
-              {{ "Member Since: " + formatTimestamp(selectedUser.createdAt) }}
-            </h4>
-            <h4>
-              {{ "Email: " + selectedUser.email }}
-            </h4>
-          </v-col>
-        </v-row>
-      </v-card>
-      <v-card class="rounded-xl d-flex flex-column pa-2" style="flex: 7">
-        <v-card-title
-          class="text-center font-weight-bold"
-          :style="{ borderBottom: `1px solid ${color.secondary}` }"
-        >
-          Chat with {{ selectedChat?.name?.split(" ")[0] || "User" }}
-        </v-card-title>
-        <v-card-text class="chat-box" @scroll="handleScroll" ref="chatBox">
+  <v-container class="pa-0" fluid>
+    <v-row class="ma-0" justify="space-around">
+      <v-col cols="12" md="7">
+        <v-card :color="color.accent" class="rounded-xl">
+          <div class="text-center text-md-h4 font-weight-bold my-2">
+            Chat List
+          </div>
+          <v-divider
+            :color="color.secondary"
+            class="border-opacity-100"
+            :thickness="2"
+          />
+          <v-list lines="two" class="overflow-auto" style="height: 78vh">
+            <v-list-item
+              v-for="chat in chatList"
+              :key="chat?.id"
+              :active="selectedChat?.id === chat.id"
+              @click="selectChat(chat)"
+              :title="chat?.name"
+              rounded="xl"
+              class="my-2"
+            >
+              <template v-slot:prepend>
+                <v-avatar
+                  :image="chat?.avatar || 'https://i.imgur.com/zsEiYcN.png'"
+                />
+              </template>
+              <template v-slot:subtitle>
+                <div class="subtitle-container">
+                  <span
+                    :style="{ color: getTextColor(chat) }"
+                    class="subtitle-text"
+                  >
+                    {{
+                      chat?.lastMessage.senderId === "admin"
+                        ? `You: ${chat?.lastMessage.text}`
+                        : chat?.lastMessage.text
+                    }}
+                  </span>
+                  <span
+                    :style="{ color: getTextColor(chat) }"
+                    class="subtitle-time"
+                  >
+                    {{
+                      chat?.lastMessage.timestamp
+                        ? new Date(
+                            chat?.lastMessage.timestamp.seconds * 1000
+                          ).toLocaleString()
+                        : " "
+                    }}
+                  </span>
+                </div>
+              </template>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="3">
+        <v-card class="rounded-xl mb-5" style="height: 30vh">
           <div
-            v-for="message in messages"
-            :key="message.timestamp"
-            class="d-flex mb-3"
-            :class="{ 'justify-end': message.senderId === 'admin' }"
+            class="position-absolute top-0 w-100"
+            :style="{ height: '40%', backgroundColor: `${color.secondary}` }"
+          ></div>
+          <v-avatar
+            class="position-absolute"
+            :image="selectedChat?.avatar || 'https://i.imgur.com/zsEiYcN.png'"
+            size="80"
+            :style="{ border: `4px solid ${color.secondary}` }"
+            style="left: 5%; top: 18%"
+          />
+          <div class="position-absolute text-md-h6" style="left: 35%; top: 28%">
+            {{ selectedUser?.displayName }}
+          </div>
+          <div
+            class="position-absolute text-md-subtitle-1"
+            style="left: 6%; bottom: 28%"
+          >
+            {{ "Member since: " + formatTimestamp(selectedUser?.createdAt) }}
+          </div>
+          <div
+            class="position-absolute text-md-subtitle-1"
+            style="left: 6%; top: 72%"
+          >
+            {{ "Email: " + selectedUser?.email }}
+          </div>
+        </v-card>
+
+        <v-card class="rounded-xl d-flex flex-column" style="height: 52vh">
+          <v-card-title
+            class="text-center font-weight-bold"
+            :style="{ borderBottom: `1px solid ${color.secondary}` }"
+          >
+            Chat with {{ selectedChat?.name?.split(" ")[0] || "User" }}
+          </v-card-title>
+          <v-card-text class="chat-box" @scroll="handleScroll" ref="chatBox">
+            <div
+              v-for="message in messages"
+              :key="message?.timestamp"
+              class="d-flex mb-3"
+              :class="{ 'justify-end': message?.senderId === 'admin' }"
+            >
+              <v-card
+                :color="
+                  message?.senderId === 'admin'
+                    ? color.secondary
+                    : 'grey-darken-3'
+                "
+                class="message-card"
+              >
+                <v-card-text
+                  class="text-left pa-2 px-3"
+                  style="white-space: pre-wrap"
+                >
+                  {{ message.text }}
+                </v-card-text>
+              </v-card>
+            </div>
+          </v-card-text>
+          <v-card-actions
+            :style="{ borderTop: `1px solid ${color.secondary}` }"
           >
             <v-card
-              :color="
-                message.senderId === 'admin' ? color.secondary : 'grey-darken-3'
-              "
-              class="message-card"
+              class="w-100 rounded-xl"
+              :style="{ borderColor: color.secondary, borderWidth: '1px' }"
+              flat
             >
-              <v-card-text
-                class="text-left pa-2 px-3"
-                style="white-space: pre-wrap"
-              >
-                {{ message.text }}
-              </v-card-text>
+              <v-textarea
+                placeholder="Message"
+                v-model="newMessage"
+                variant="solo-filled"
+                rows="1"
+                max-rows="3"
+                auto-grow
+                hide-details
+                density="compact"
+                @keydown.enter="handleEnter"
+              />
             </v-card>
-          </div>
-        </v-card-text>
-        <v-card-actions :style="{ borderTop: `1px solid ${color.secondary}` }">
-          <v-card
-            class="w-100 rounded-xl"
-            :style="{ borderColor: color.secondary, borderWidth: '1px' }"
-            flat
-          >
-            <v-textarea
-              placeholder="Message"
-              v-model="newMessage"
-              variant="solo-filled"
-              rows="1"
-              max-rows="3"
-              auto-grow
-              hide-details
-              density="compact"
-              @keydown.enter="handleEnter"
-            />
-          </v-card>
-          <v-icon
-            class="custom-icon cursor-pointer mx-2"
-            :style="{ color: color.secondary }"
-            @click="sendMessage"
-          >
-            fa-solid fa-paper-plane
-          </v-icon>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+            <v-icon
+              class="custom-icon cursor-pointer mx-2"
+              :style="{ color: color.secondary }"
+              @click="sendMessage"
+            >
+              fa-solid fa-paper-plane
+            </v-icon>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
@@ -174,7 +189,7 @@ const formatTimestamp = (timestamp) => {
 };
 
 const scrollToBottom = () => {
-  if (chatBox.value)
+  if (chatBox.value?.$el)
     chatBox.value.$el.scrollTop = chatBox.value.$el.scrollHeight;
 };
 
@@ -196,7 +211,7 @@ const handleEnter = (event) => {
 };
 
 const handleScroll = async () => {
-  if (chatBox.value.$el.scrollTop === 0) {
+  if (chatBox.value?.$el.scrollTop === 0) {
     const previousScrollHeight = chatBox.value.$el.scrollHeight;
     await store.dispatch("adminChat/loadMoreMessages", selectedChat.value.id);
     chatBox.value.$el.scrollTop =
@@ -223,10 +238,6 @@ onMounted(async () => {
 </script>
 
 <style scoped>
-.chat-list {
-  max-height: 78vh;
-  overflow-y: auto;
-}
 .subtitle-container {
   display: flex;
   justify-content: space-between;
@@ -253,9 +264,7 @@ onMounted(async () => {
   transform: scale(1.1) rotate(15deg);
 }
 .chat-box {
-  height: 40vh;
-  max-height: 40vh;
-  height: 100%;
+  max-height: 37.5vh;
   overflow-y: auto;
   padding: 16px;
   display: flex;
--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,42 +1,43 @@
 <template>
-  <v-container fluid class="d-flex" style="height: 90vh">
-    <v-row class="flex-grow-1">
-      <v-col cols="2" class="d-flex flex-column" style="gap: 12px">
+  <v-container class="pa-0" fluid>
+    <v-row class="ma-0">
+      <v-col cols="12" md="2">
         <v-card
-          class="rounded-xl d-flex flex-column justify-center align-center"
-          style="height: 30vh"
+          class="rounded-xl mb-5 d-flex flex-column justify-center align-center"
+          :color="color.accent"
+          :style="{ border: `2px solid ${color.secondary}`, height: '30vh' }"
         >
-          <h2>Logeco Admin</h2>
           <v-avatar
-            size="130"
+            size="100"
             image="https://i.imgur.com/Uibhtmc.png"
             :style="{ border: `2px solid ${color.secondary}` }"
-          >
-          </v-avatar>
+          />
+          <div class="text-md-h5 my-3">Logeco Admin</div>
 
           <v-btn
-            class="rounded-pill mt-2"
+            size="small"
+            class="rounded-pill"
             :color="color.secondary"
-            density="compact"
             @click="handleLogout"
-            >Logout</v-btn
+            >Log out</v-btn
           >
         </v-card>
-        <v-card class="pa-4 d-flex flex-column rounded-xl" variant="tonal">
+        <v-card class="rounded-xl pa-4" variant="tonal">
           <v-btn
             v-for="item in navItems"
             :key="item.key"
             block
+            :prepend-icon="item.icon"
             :color="item.key === selectedComponentKey ? color.secondary : ''"
-            class="rounded-pill py-6 my-2"
+            class="rounded-xl my-2 py-6"
+            variant="flat"
             @click="selectComponent(item.key)"
           >
-            <v-icon class="mr-4">{{ item.icon }}</v-icon>
             {{ item.title }}
           </v-btn>
         </v-card>
       </v-col>
-      <v-col cols="10">
+      <v-col cols="12" md="10">
         <component :is="selectedComponent"></component>
       </v-col>
     </v-row>
